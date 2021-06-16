e1281e74b5ccae7136d83b8bd38532d65299f2b0
@reroes <reroes799@gmail.com>
2021-06-16 14:54:34 -0500

    ejemplos para clase 11

---
+++ EjemploHerencia/Herencias/src/herencias2/Principal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencias2;
+
+/**
+ *
+ * @author reroes
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Estudiante e = new Estudiante("René", "Elizalde", 33, 100.2);
+        System.out.println(e);
+        // Un docente hereda de una Persona y adicionalmente tiene 
+        // la característia sueldo
+        Docente d = new Docente("Luis", "Alvarez", 40, 1000); // falta implementar
+        System.out.println(d);
+        
+        // Un policia hereda de una Persona y adicionalmente tiene 
+        // la característia rango
+        Policia p = new Policia("ALEX", "MENDOZA", 35, "Cabo"); // falta implementar
+        System.out.println(p);
+        
+    }
+    
+}
